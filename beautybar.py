2196a49adad66e313a2d8b9d4bc9e08dacb446fe
ippe <ippe@pohtoshop.(none)>
2009-09-30 14:57:10 +0300

    basic skeleton for django / google appengine application

---
+++ beautybar.py
@@ -0,0 +1,23 @@
+import os
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+    
+    def get(self):
+        values = {
+            'name' : 'ilpo'
+            }
+        path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+        # self.response.out.write("hello world!!!")
+        self.response.out.write(template.render(path, values))
+
+application = webapp.WSGIApplication([('/', MainPage)],
+                                      debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
