c66643cf83a2f209879795751ba80f920a89f149
ippe <ippe@pohtoshop.(none)>
2009-10-01 13:26:19 +0300

    values get from ui

--- ui/mainpage.py
+++ ui/mainpage.py
@@ -2,11 +2,25 @@ import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
+class Item(object):
+
+    def __init__(self, name, value):
+        self.name  = name
+        self.value = value
+
 class MainPage(webapp.RequestHandler):
     
     def get(self):
         values = {
-            'name' : 'ilpo'
+            'name'  : 'ilpo',
+            'items' : [ Item("Ilpo", "28"),
+                        Item("Ilpo", "28"),
+                        Item("Lars", "24"),
+                        Item("Lars", "24"),
+                        Item("Sande", "27"),
+                        Item("Sande", "27"),
+                        ],
             }
-        path = os.path.join(os.path.dirname(__file__), '../templates/index.html')
+        path = os.path.join(os.path.dirname(__file__), 
+                            '../templates/index.html')
         self.response.out.write(template.render(path, values))
