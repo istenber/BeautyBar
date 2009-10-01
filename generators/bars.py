c1bac0e544058f7e206df7c0daa7c17361b3b54f
ippe <ippe@pohtoshop.(none)>
2009-10-01 21:55:30 +0300

    first working version!

--- generators/bars.py
+++ generators/bars.py
@@ -2,52 +2,49 @@
 
 from xml.dom import minidom
 
-template="bars/template.svg"
+# TODO: read path with some other way?
+template="generators/bars/template.svg"
 
 class Bars(object):
+
     def __init__(self):
         self.values = []
+        # TODO: change to english
         self.id_prefixes = [ "arvo", "aika", "pylvas" ]
     def scale(self, min, max):
-        step = (max - min) / 5
-        self.scale = range(min, max + step, step)
+        self.step = (max - min) / 5
+        self.scale = range(min, max + self.step, self.step)
         # print "# scale " + str(self.scale)
     def add(self, name, value):
         self.values.append([name, value])
     def output(self):
         self.doc = minidom.parse(template)
-        # self._generate_output()        
-        return "hello" # self.output
+        self._generate_output()        
+        return self.output
         # with open("/tmp/bars_output.svg", 'w') as f:
         #     f.write(self.output)
         # self._debug()
 
     def _process_pylvas(self, index, elem):
         # print "# process_pylvas: " + str(elem)
-        # TODO: stepping
-        # step = (max - min) / 5
-        # x = int(self.values[index][1]) * 73 / 10 # step
-        y = 435 - (int(self.values[index][1]) * 10 * 73 / 100)
+        y = 435 - (int(self.values[index][1]) * self.step * 73 / 100)
         h = 433 - y
         # print "\ty: " + str(y)
         # print "\th: " + str(h)
         elem.setAttribute("height", str(h))
         elem.setAttribute("y", str(y))
-
     def _process_aika(self, index, elem):
         # print "# process_aika: " + str(elem)
         tspan = elem.childNodes[0]
         old_txt = tspan.childNodes[0]
         new_txt = self.doc.createTextNode(str(self.values[index][0]))
         tspan.replaceChild(new_txt, old_txt)
-
     def _process_arvo(self, index, elem):
         # print "# process_arvo: " + str(elem)
         tspan = elem.childNodes[0]
         old_txt = tspan.childNodes[0]
         new_txt = self.doc.createTextNode(str(self.scale[index]))
         tspan.replaceChild(new_txt, old_txt)
-
     def _elem(self, elem):
         # print "elem: " + str(elem.nodeValue)
         try:
@@ -63,15 +60,9 @@ class Bars(object):
             pass
         for child in elem.childNodes:
             self._elem(child)
-
     def _generate_output(self):
         self._elem(self.doc)
         self.output = self.doc.toprettyxml()
-
-    def _read_template(self):
-        self.template = ""
-        with open(template, 'r') as f:
-            self.template = f.read()
 
 def main():
     bars = Bars()
--- ui/output_image.py
+++ ui/output_image.py
@@ -4,14 +4,14 @@ from generators.bars import Bars
 class OutputImage(webapp.RequestHandler):
     
     def get(self):
-        # self.response.headers['Content-Type'] = "image/svg+xml"
+        self.response.headers['Content-Type'] = "image/svg+xml"
         bars = Bars()
         bars.scale(0, 50)
-        bars.add("Ilpo", 28)
+        bars.add("Ilpo", 40)
         bars.add("Lasse", 24)
         bars.add("Sanna", 27)
         bars.add("Ilpo", 28)
         bars.add("Lasse", 24)
         bars.add("Sanna", 27)
         self.response.out.write(bars.output())
-        # self.response.out.write(bars.output())
+
