27faaf03e8af4d0604ab44341baa5aee37501f2d
ippe <ippe@pohtoshop.(none)>
2009-10-01 23:00:09 +0300

    session cookie added

--- ui/mainpage.py
+++ ui/mainpage.py
@@ -1,4 +1,5 @@
 import os
+import random
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
@@ -11,16 +12,25 @@ class Item(object):
 class MainPage(webapp.RequestHandler):
     
     def get(self):
+        if self.request.cookies.has_key("session"):            
+            session = str(self.request.cookies["session"])
+        else:
+            session = str(random.randint(1, 10000000))
+
+        debug = "( session:" + str(session) + " )"
+        # debug = "(" + str(dir(self.response)) + ")"
+        items = [ Item("Ilpo", "28"),
+                  Item("Ilpo", "28"),
+                  Item("Lars", "24"),
+                  Item("Lars", "24"),
+                  Item("Sande", "27"),
+                  Item("Sande", "27"),
+                  ]
         values = {
-            'name'  : 'ilpo',
-            'items' : [ Item("Ilpo", "28"),
-                        Item("Ilpo", "28"),
-                        Item("Lars", "24"),
-                        Item("Lars", "24"),
-                        Item("Sande", "27"),
-                        Item("Sande", "27"),
-                        ],
+            'items' : items,
+            'debug' : debug
             }
         path = os.path.join(os.path.dirname(__file__), 
                             '../templates/index.html')
+        self.response.headers['Set-Cookie'] = "session=" + session
         self.response.out.write(template.render(path, values))
