c4bc8aefd79aba5a6ac3cf33eeff2c24bf83d61e
ippe <ippe@pohtoshop.(none)>
2009-10-07 16:16:34 +0300

    now we generate random image after every change

--- static/js/beautybar.js
+++ static/js/beautybar.js
@@ -2,12 +2,14 @@
 var data = {};
 
 data.modify = function(x, y, val) {
-    params = "x=" + x + "&y=" + y + "&val=" + val;
+    var params = "x=" + x + "&y=" + y + "&val=" + val;
     new Ajax.Request('/modify_data?' + params, {
 	    method    : 'get',
 	    onSuccess : function(out) {
 		$('debug').update("[" + x + "," + y + "]: " + val + 
 				  " - " + out.responseText);
+		var rnd = Math.floor(Math.random() * 1000000)
+		$('output_image').update("<object data=\"/output_image?\" type=\"image/svg+xml\" height=\"200\" width=\"300\" />");
 	    },
 	    onFailure : function() { 
 		$('debug').update("ajax failed");
