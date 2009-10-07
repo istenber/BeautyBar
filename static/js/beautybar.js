3ccb9f05bc41e63c43aa36f4bd4715f1c58b2ee2
ippe <ippe@pohtoshop.(none)>
2009-10-07 17:38:41 +0300

    image is updated only once a second maximum

--- static/js/beautybar.js
+++ static/js/beautybar.js
@@ -8,8 +8,7 @@ data.modify = function(x, y, val) {
 	    onSuccess : function(out) {
 		$('debug').update("[" + x + "," + y + "]: " + val + 
 				  " - " + out.responseText);
-		var rnd = Math.floor(Math.random() * 1000000)
-		$('output_image').update("<object data=\"/output_image?\" type=\"image/svg+xml\" height=\"200\" width=\"300\" />");
+		start_update_timer();
 	    },
 	    onFailure : function() { 
 		$('debug').update("ajax failed");
@@ -24,3 +23,21 @@ fm = function(val) {
 	data.modify(2, val.substr(5), $(val).getValue());
     }
 };
+
+var timer_on = 0;
+
+update_image = function() {
+    $('output_image').update(
+  "<object data=\"/output_image?" + Math.floor(Math.random() * 1000000) + 
+  "\" type=\"image/svg+xml\" height=\"200\" width=\"300\" />");
+    timer_on = 0;
+    $('debug').update("image updated");
+};
+
+start_update_timer = function() {
+    if(timer_on == 0) {
+	timer_on = 1;
+	setTimeout("update_image();", 1000);
+    }
+};
+
