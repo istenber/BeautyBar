40c3dafc5a688e4a88d6d3e54cda09dcc9ff9099
Ilpo Stenberg <sankari@turska.(none)>
2009-11-05 14:19:52 +0200

    some refactoring to remove duplicate code

--- ui/extra_pages.py
+++ ui/extra_pages.py
@@ -3,42 +3,33 @@ import random
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
-class LearnPage(webapp.RequestHandler):
+class BasePage(webapp.RequestHandler):
     
+    def _get_values(self):
+        """Override me!"""
+        
     def get(self):
-        # TODO: move to general functions or derive all pages
-        #       from SessionPage?
         if self.request.cookies.has_key("session"):            
             session = str(self.request.cookies["session"])
         else:
             # TODO: should use data_operations.py / CleanData
             session = str(random.randint(1, 10000000))
-        debug = "(none)"
-        values = {
-            'template'   : "learnpage.html",
-            'debug'      : debug,
-            }
-        path = os.path.join(os.path.dirname(__file__), 
-                            '../templates/base.html')
-        self.response.headers['Set-Cookie'] = "session=" + session
-        self.response.out.write(template.render(path, values))
 
-class InfoPage(webapp.RequestHandler):
-    
-    def get(self):
-        # TODO: move to general functions or derive all pages
-        #       from SessionPage?
-        if self.request.cookies.has_key("session"):            
-            session = str(self.request.cookies["session"])
-        else:
-            # TODO: should use data_operations.py / CleanData
-            session = str(random.randint(1, 10000000))
-        debug = "(none)"
-        values = {
-            'template'   : "infopage.html",
-            'debug'      : debug,
-            }
+        values = self._get_values()
+        if not 'debug' in values:
+            values['debug'] = "(none)"
+        #    'template'   : "learnpage.html", # TODO:
         path = os.path.join(os.path.dirname(__file__), 
                             '../templates/base.html')
         self.response.headers['Set-Cookie'] = "session=" + session
-        self.response.out.write(template.render(path, values))
+        self.response.out.write(template.render(path, values))    
+
+class LearnPage(BasePage):
+    
+    def _get_values(self):
+        return { 'template' : "learnpage.html" }
+
+class InfoPage(BasePage):
+    
+    def _get_values(self):
+        return { 'template' : "infopage.html" }
