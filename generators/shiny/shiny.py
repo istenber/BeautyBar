8c28c59cc74fef193dead148506084de0f205e6e
Ilpo Stenberg <sankari@turska.(none)>
2009-11-11 15:52:51 +0200

    refactored to use some objects

--- generators/shiny/shiny.py
+++ generators/shiny/shiny.py
@@ -12,8 +12,6 @@ from svgfig import *
 
 def convert_color(style, sub_func):
     str_in = style["style"]
-    def _sub_func(c):
-        pass
     def _repl(m):
         color = m.group(0)
         # print "color: " + str(color)
@@ -25,11 +23,25 @@ def convert_color(style, sub_func):
                      str_in)
     style["style"] = str_out
 
-def blue_to_green(style):
-    convert_color(style, lambda c: c[1:3] + c[5:7] + c[3:5])
-
 def red_to_blue(style):
     convert_color(style, lambda c: c[5:7] + c[3:5] + c[1:3])
+
+def red_to_green(style):
+    convert_color(style, lambda c: c[3:5] + c[1:3] + c[5:7])
+
+class Bar(object):
+    def __init__(self, template):
+        self.bar = template.clone()
+    def set_color(self, color):
+        if color == "red": return
+        b = self.bar
+        styles = (b[0], b[1, 0], b[1, 1], b[1, 2])
+        if color == "blue": f = red_to_blue
+        if color == "green": f = red_to_green
+        for style in styles:
+            f(style)    
+    def set_pos(self, pos):
+        self.bar["transform"] = "translate(" + pos + ")"
 
 def main():
     """ 
@@ -41,33 +53,27 @@ def main():
     attr = svg.attr
     filters = svg[0]
     bar = svg[3, 0]
-    styles = (bar[0], bar[1, 0], bar[1, 1], bar[1, 2])
-    # print str(bar)
-    rbar = bar.clone()
 
-    for style in styles:
-        red_to_blue(style)
-    svg.save("blue.svg")
-    bbar = bar.clone()
+    rbar = Bar(bar)
+    rbar.set_color("red")
+    rbar.set_pos("-287, -386")
 
-    for style in styles:
-        blue_to_green(style)
-    svg.save("green.svg")
-    gbar = bar.clone()
+    bbar = Bar(bar)
+    bbar.set_color("blue")
+    bbar.set_pos("-187, -386")
 
-    # print "r: " + str(rbar)
-    # print "g: " + str(bbar)
-    # print "b: " + str(gbar)
+    gbar = Bar(bar)
+    gbar.set_color("green")
+    gbar.set_pos("-87, -386")
 
-    rbar["transform"] = "translate(-287,-386)"
-    bbar["transform"] = "translate(-187,-386)"
-    gbar["transform"] = "translate(-87,-386)"
+    gbar2 = Bar(bar)
+    gbar2.set_color("green")
+    gbar2.set_pos("23, -386")
 
-    scaler = SVG("g", rbar, bbar, gbar, transform="scale(0.5)")
-
-    # all = SVG("svg", filters, rbar, bbar, gbar)
+    scaler = SVG("g", 
+                 rbar.bar, bbar.bar, gbar.bar, gbar2.bar,
+                 transform="scale(0.5)")
     all = SVG("svg", filters, scaler)
-    # print attr
     attr["height"] = 200
     attr["width"] = 300
     all.attr = attr
