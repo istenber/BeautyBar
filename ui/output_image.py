4ef800f6ff58b5914375ab4b0c87fa0b339bdad2
Ilpo Stenberg <sankari@turska.(none)>
2009-11-13 19:12:47 +0200

    output image uses now generator stored in database

--- ui/output_image.py
+++ ui/output_image.py
@@ -2,19 +2,19 @@ import logging
 
 from google.appengine.ext import webapp
 from model.data import Item, Data
-from ui.dao import ItemDAO, DataDAO
+from ui.dao import ItemDAO, DataDAO, GeneratorDAO
 
 default_generator="shiny"
 
-class OutputImage(webapp.RequestHandler):    
+class OutputImage(webapp.RequestHandler):
     def get(self):
-        name = self.request.get("name")
-        if name == "": name = default_generator
-        logging.debug("# generator name(" + name + ")")
         self.response.headers['Content-Type'] = "image/svg+xml"
         if self.request.cookies.has_key("session"):            
             session = str(self.request.cookies["session"])
             data = DataDAO.load(session)
+            name = GeneratorDAO.load(session)
+        if name is None: name = default_generator
+        logging.debug("# generator name(" + name + ")")
         self.response.out.write(self._get_generator(data, name))
 
     def _get_generator(self, data, name):
