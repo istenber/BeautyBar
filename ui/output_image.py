8c22cf4d6dae6b7ba22a6b65143525242ee04447
Ilpo Stenberg <sankari@turska.(none)>
2009-11-13 17:48:27 +0200

    major refactoring to remove duplicate code

--- ui/output_image.py
+++ ui/output_image.py
@@ -4,42 +4,28 @@ from google.appengine.ext import webapp
 from model.data import Item, Data
 from ui.dao import ItemDAO, DataDAO
 
-class OutputImage(webapp.RequestHandler):
-    
+default_generator="shiny"
+
+class OutputImage(webapp.RequestHandler):    
     def get(self):
-        generator = self.request.get("generator")
-        if generator == "":
-            generator = "bars"
-        logging.info("generator(" + generator + ")")
+        name = self.request.get("name")
+        if name == "": name = default_generator
+        logging.debug("# generator name(" + name + ")")
         self.response.headers['Content-Type'] = "image/svg+xml"
         if self.request.cookies.has_key("session"):            
             session = str(self.request.cookies["session"])
             data = DataDAO.load(session)
-        if hasattr(self, "_" + generator):
-            f = getattr(self, "_" + generator)
-        self.response.out.write(f(data))
+        self.response.out.write(self._get_generator(data, name))
 
-    def _bars(self, data):
-        x = "bars"
-        classname = x.capitalize()
-        filename = "generators." + x
+    def _get_generator(self, data, name):
+        logging.debug("# output_image/_get_generator [" + name + "]")
+        # TODO: verify that generator exists...
+        classname = name.capitalize()
+        filename = "generators." + name
         import_cmd = "from " + filename + " import " + classname
         logging.info(import_cmd)
         exec(import_cmd)
-        bars = Bars()
+        bars = eval(classname + "()")
         bars.scale(0, 50)
         data.to_generator(bars)
         return bars.output()
-
-    def _paper(self, data):
-        x = "paper"
-        classname = x.capitalize()
-        filename = "generators." + x
-        import_cmd = "from " + filename + " import " + classname
-        logging.info(import_cmd)
-        exec(import_cmd)
-        paper = Paper()
-        paper.scale(0, 50)
-        data.to_generator(paper)
-        return paper.output()
-
