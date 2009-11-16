30ca2375436d2592cb6845e4a529baca858a3afa
Ilpo Stenberg <sankari@turska.(none)>
2009-11-16 21:56:19 +0200

    attribute table for current generator made, although it does not contain real values yet. but as there is no attributes in generators yet either, it doesn't matter.

--- ui/ajax_generator.py
+++ ui/ajax_generator.py
@@ -4,6 +4,7 @@ import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from ui.dao import GeneratorDAO
+from model.generator_factory import GeneratorFactory
 
 # TODO: refactor, combine with ajax_modify
 class AjaxGenerator(webapp.RequestHandler):
@@ -18,13 +19,25 @@ class AjaxGenerator(webapp.RequestHandler):
 
 class AjaxAttributes(webapp.RequestHandler):
     def get(self):
-        # logging.info("# Attribute table called.")
         # TODO: handle missing args and cookie
         if self.request.cookies.has_key("session"):            
             session = str(self.request.cookies["session"])
-        values = []
+        gen_name = GeneratorDAO.load(session)
+        gf = GeneratorFactory().instance()
+        generator = gf.get_generator(gen_name + ".py")
+        values = {}
+        parts = []
+        for attr in generator.attributes():
+            # TODO: select parts for correct attribute type
+            #       old value, name and how to handle...
+            parts.append(self._part_Color())
+        values["parts"] = parts
         self.response.headers['Content-Type'] = "text/html"
         path = os.path.join(os.path.dirname(__file__), 
                             '../templates/attribute_table.html')
         self.response.out.write(template.render(path, values))
     
+    def _part_Color(self):
+        return ("<tr>\n<td>Color:</td>\n<td>" +
+                "<input type=\"text\" name=\"bgcolor\" value=\"red\">\n" +
+                "</td></tr>")
