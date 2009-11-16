145aa50ec70c305a9a5e263c59e2548298cc7942
Ilpo Stenberg <sankari@turska.(none)>
2009-11-16 20:48:01 +0200

    refactor / bug fix

--- ui/dao.py
+++ ui/dao.py
@@ -15,7 +15,8 @@ class GeneratorDAO(db.Model):
         if dao is None:
             # TODO: we have now many names for session number,
             #       name, session, data_ref - fix to one
-            dao = GeneratorDAO(session = session)
+            dao = GeneratorDAO()
+        dao.session = session
         dao.generator = generator
         dao.put()
 
@@ -43,8 +44,9 @@ class ItemDAO(db.Model):
                           " row = :1 AND data_ref = :2", 
                           str(c), str(ref)).get()
         if dao is None:
-            dao = ItemDAO(name = item.name,
-                          data_ref = str(ref))
+            dao = ItemDAO()
+        dao.name = item.name
+        dao.data_ref = str(ref)
         dao.value = str(item.value)        
         dao.row = str(c)
         dao.put()
@@ -74,7 +76,8 @@ class DataDAO(db.Model):
         dao = db.GqlQuery("SELECT * FROM DataDAO WHERE name = :1",
                         name).get()
         if dao is None:
-            dao = DataDAO(name = str(name))
+            dao = DataDAO()
+        dao.name = str(name)
         dao.title = "no title"
         dao.min = str(data.min)
         dao.max = str(data.max)
