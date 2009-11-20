fa7792365e9a306e992b9da7587e2e99bb89506e
Ilpo Stenberg <sankari@turska.(none)>
2009-11-20 14:53:09 +0200

    wow. I can see mainpage again, so it is fixed?

--- ui/mainpage.py
+++ ui/mainpage.py
@@ -1,23 +1,22 @@
 from model.data import Item, Data
-from ui.dao import ItemDAO, DataDAO, GeneratorDAO
 from model.generator_factory import GeneratorFactory
 from ui.basepage import BasePage
 
 class MainPage(BasePage):
     
     def _get_values(self):
-        data = DataDAO.load(self.session)
-        items = data.as_list()
+        items = self.session.data.as_list()
+        cur_gen = self.session.style.get_active_generator()
         # debug = "( data:" + str(data.as_list()) + " )"
-        debug = "( session:" + str(self.session) + " )"
+        debug = "( session:" + str(self.session.name) + " )"
         # debug = "(" + str(dir(self.response)) + ")"
         values = {
             'items'      : items,
             'generators' : GeneratorFactory().list(),
             'debug'      : debug,
-            'cur_gen'    : GeneratorDAO.load(self.session),
-            'r_min'      : data.min,
-            'r_max'      : data.max,            
+            'cur_gen'    : cur_gen,
+            'r_min'      : self.session.data.min,
+            'r_max'      : self.session.data.max,
             }
         return values
 
