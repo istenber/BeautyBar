53d2a89f8e16e672da5e100d93e4c38164c5e501
Ilpo Stenberg <sankari@turska.(none)>
2009-11-20 14:56:46 +0200

    debugging removed

--- ui/mainpage.py
+++ ui/mainpage.py
@@ -7,13 +7,9 @@ class MainPage(BasePage):
     def _get_values(self):
         items = self.session.data.as_list()
         cur_gen = self.session.style.get_active_generator()
-        # debug = "( data:" + str(data.as_list()) + " )"
-        debug = "( session:" + str(self.session.name) + " )"
-        # debug = "(" + str(dir(self.response)) + ")"
         values = {
             'items'      : items,
             'generators' : GeneratorFactory().list(),
-            'debug'      : debug,
             'cur_gen'    : cur_gen,
             'r_min'      : self.session.data.min,
             'r_max'      : self.session.data.max,
