873b4c5328d8669d0740c73bded408800f0ee9da
Ilpo Stenberg <sankari@turska.(none)>
2009-11-27 12:38:11 +0200

    chart creation part refactored to use model.generator.build_chart -method

--- ui/output_image.py
+++ ui/output_image.py
@@ -3,15 +3,10 @@ import logging
 from google.appengine.ext import webapp
 from ui.dao import DAO
 from model.session import Session
-from model.generator_factory import GeneratorFactory
+
 
 # TODO: refactor session handling into one place
 class OutputImage(webapp.RequestHandler):
-
-    def _unquote(self, string):
-        if string.startswith('"') : string = string[1:]
-        if string.endswith('"') : string = string[:-1]
-        return string
 
     def get(self):
         # TODO: use content type from output
@@ -20,15 +15,5 @@ class OutputImage(webapp.RequestHandler):
         if name == "" and self.request.cookies.has_key("session"):
             name = str(self.request.cookies["session"])
         self.session = DAO.load(name=name, class_name="Session")
-        self.response.out.write(self._get_generator())
-
-    def _get_generator(self):
         g = self.session.style.get_active_generator()
-        gf = GeneratorFactory().instance()
-        chart = gf.get_generator(g.name + ".py")
-        for attr in chart.attributes():
-            v = self._unquote(g.get_attribute(attr.x_name()).value)
-            if v != "":
-                attr.set(v)
-        self.session.data.to_generator(chart)
-        return chart.output()
+        self.response.out.write(g.build_chart(self.session.data))
