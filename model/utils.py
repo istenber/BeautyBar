58b700e69e4ec1bf97fa63e7532539188bb81af2
Ilpo Stenberg <sankari@turska.(none)>
2009-11-29 00:15:46 +0200

    first "doctest" created.

--- model/utils.py
+++ model/utils.py
@@ -1,5 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Here are some general use utils that are used throughout the code.
+"""
 
 def unquote(string):
+    """Removes double quotes from string
+
+    >>> unquote('"hello"')
+    'hello'
+
+    If there is only one double quote let's not remove it.
+
+    >>> unquote('"hel')
+    '"hel'
+    """
+
     if string.startswith('"') and string.endswith('"'):
         return string[1:-1]
     return string
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
