c1b1c271c66ca4201599c3016cec03c71c46854e
Ilpo Stenberg <sankari@turska.(none)>
2009-12-11 15:15:38 +0200

    lets not obfurscate css and javascript - it only makes debugging harder

--- send-to-google.sh
+++ send-to-google.sh
@@ -3,10 +3,7 @@
 APP_FILE=app.yaml
 TMP_FOLDER=/tmp/export
 APP_ENGINE_PATH=/home/sankari/dev/google_appengine/
-COMPRESS_JAR=/home/sankari/dev/beautybar/utils/yuicompressor-2.4.2.jar
 GIT_REPO=file:///home/sankari/dev/beautybar/.git/
-JS_FILE=beautybar/static/js/beautybar.js
-CSS_FILE=beautybar/static/css/screen.css
 
 # increase version number in app.yaml
 NEW_VERSION=`awk '/^version/ {print $2+1;}' ${APP_FILE}`
@@ -33,16 +30,6 @@ rm -rf ${TMP_FOLDER}/beautybar/tests
 rm ${TMP_FOLDER}/beautybar/README
 rm ${TMP_FOLDER}/beautybar/generators/skel.py
 rm ${TMP_FOLDER}/beautybar/send-to-google.sh
-
-# obfuscate and compress javascript code
-TMP_FILE=/tmp/js.compressed
-java -jar ${COMPRESS_JAR} --type js -o ${TMP_FILE} ${TMP_FOLDER}/${JS_FILE}
-mv ${TMP_FILE} ${TMP_FOLDER}/${JS_FILE}
-
-# obfuscate and compress css code
-TMP_FILE=/tmp/css.compressed
-java -jar ${COMPRESS_JAR} --type css -o ${TMP_FILE} ${TMP_FOLDER}/${CSS_FILE}
-mv ${TMP_FILE} ${TMP_FOLDER}/${CSS_FILE}
 
 # send to google
 ${APP_ENGINE_PATH}/appcfg.py update ${TMP_FOLDER}/beautybar
