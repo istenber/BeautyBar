4ca44981706a187c974202ef94a56610f5d6a839
Ilpo Stenberg <sankari@turska.(none)>
2009-12-24 17:29:14 +0100

    skel rewritten

--- generators/skel.py
+++ generators/skel.py
@@ -1,17 +1,25 @@
-#!/usr/bin/env python
+import logging
 
-from gui_interface import GuiInterface
+from base import BaseGenerator
 
-class Empty(GuiInterface):
-    def name(self):
-        return "Empty generator"
-    
-    def attributes(self):
-        return []
+class Skel(BaseGenerator):
+    def __init__(self):
+        pass
 
-def main():
-    e = Empty()
-    print "name -> " + e.name()
+    # Process interface
+    def set_range(self, min, max):
+        pass
+    def add_row(self, name, value, index):
+        pass
+    def output(self):
+        pass
 
-if __name__ == "__main__":
-    main()
+    # Gui interface
+    def get_description(self):
+        pass
+    def get_ui_name(self):
+        pass
+    def get_attributes(self):
+        pass
+    def get_rating(self):
+        pass
