65cd768c83c71e87e5bd4b2e6af7ca734002f229
Ilpo Stenberg <sankari@turska.(none)>
2010-01-14 13:54:15 +0100

    lots of refactorings...

--- model/decorator.py
+++ model/decorator.py
@@ -16,8 +16,16 @@ class Decorator(object):
         # needed if cStringIO is used
         utf16_str = self.generator.output().encode("UTF-16")
         self.svg = load_stream(StringIO(utf16_str))
-        self.width = float(self.svg.attr['width'])
-        self.height = float(self.svg.attr['height'])
+        self.width = self._to_pixels(self.svg.attr['width'])
+        self.height = self._to_pixels(self.svg.attr['height'])
+
+    def _to_pixels(self, string):
+        if "px" in string:
+            string = string.replace("px", "")
+        try:
+            return float(string)
+        except ValueError:
+            return 0
 
     def resize_str(self, resize_str):
         """Get new image size as string: \"300x200\""""
@@ -36,19 +44,26 @@ class Decorator(object):
         return self.scale_xy(scale, scale)
 
     def _done_before(self):
-        if hasattr(self, "_db_flag"):
-            return self._db_flag
-        try:
-            self._db_flag = (self.svg[1]["id"] == "full_scaler")
-        except KeyError:
-            self._db_flag = False
+        if not hasattr(self, "_db_flag"):
+            try:
+                self._db_flag = (self.svg[self._ip()]["id"] == "full_scaler")
+            except KeyError:
+                self._db_flag = False
         return self._db_flag
+
+    def _ip(self): # items position, defs + 1
+        if not hasattr(self, "_ipos_flag"):
+            if self.svg[0].t == "defs":
+                self._ipos_flag = 1
+            else:
+                self._ipos_flag = 0
+        return self._ipos_flag
 
     def scale_xy(self, sx, sy, move=True):
         """We can scale x axis and y axis different amount, and
         we might want to move figure to center after scaling"""
         if self._done_before():
-            old_matrix = self.svg[1]["transform"][7:].split(",")
+            old_matrix = self.svg[self._ip()]["transform"][7:].split(",")
             sx = sx * float(old_matrix[0])
             sy = sy * float(old_matrix[3])
         if move:
@@ -61,14 +76,13 @@ class Decorator(object):
                   ", " + str(cx) + ", " + str(cy) + ")")
         # logging.info("# scale/" + matrix)
         if self._done_before():
-            self.svg[1]["transform"] = matrix
+            self.svg[self._ip()]["transform"] = matrix
         else:
-            scaler = SVG("g", id="full_scaler",
-                         transform=matrix)
-            for e in self.svg[1:]:
+            scaler = SVG("g", id="full_scaler", transform=matrix)
+            for e in self.svg[self._ip():]:
                 scaler.append(e)
-            self.svg[1] = scaler
-            self.svg[2:] = ""
+            self.svg[self._ip()] = scaler
+            self.svg[(self._ip() + 1):] = ""
         self.width = 300 * sx
         self.height = 200 * sy
 
@@ -85,8 +99,6 @@ class Decorator(object):
 
     def resize(self, width, height):
         self.scale_xy(width / self.width, height / self.height, move=False)
-        self.width = width
-        self.height = height
 
     def output(self):
         # self.scale(0.8)
