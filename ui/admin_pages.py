9bb689c950bf92612a55b4afdfd6cd05469056b2
Ilpo Stenberg <sankari@turska.(none)>
2010-01-22 12:12:32 +0100

    admin pages fixed to use new db model

--- ui/admin_pages.py
+++ ui/admin_pages.py
@@ -4,7 +4,7 @@ import os
 from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 from google.appengine.ext import db
-from ui.dao import SessionDAO
+import ui.dao
 
 
 max_sessions=100
@@ -14,9 +14,9 @@ class ViewSessions(webapp.RequestHandler):
 
     def get(self):
         # TODO: make paging
-        daos = SessionDAO.all().fetch(max_sessions)        
+        sessions = ui.dao.Session.all().fetch(max_sessions)        
         values = {
-            'sessions' : daos # TODO: should use Session, not SessionDAO
+            'sessions' : sessions
             }
         path = os.path.join(os.path.dirname(__file__),
                             '../templates/view_sessions.html')
