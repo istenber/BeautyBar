2befe6cf293f69842c042249127654c844f4459e
Ilpo Stenberg <sankari@turska.(none)>
2010-02-02 22:31:24 +0100

    by default we test all files, but we can give individual file as param

--- test_model.sh
+++ test_model.sh
@@ -2,10 +2,23 @@
 
 # TODO: all model files should be fixed to work with this...
 
-PYTHONPATH=$PWD python2.5 model/data.py
-exit 0
+test() {
+    # echo "Testing $1"
+    PYTHONPATH=$PWD python2.5 $1
+}
+
+if [ $# -ne 0 ]; then
+    if [ -f $1 ]; then
+	test $1
+	exit 0
+    else
+	echo "Missing file: $1"
+	exit 2
+    fi
+fi
 
 for python_file in `ls model | grep '\.py$'`; do
-  # echo "Testing $python_file"
-  PYTHONPATH=$PWD python2.5 model/$python_file
+    test model/$python_file
 done
+
+exit 0
