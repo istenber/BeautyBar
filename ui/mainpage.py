5bfc973d2c781b579cb06437e0eb8301b4b5fdb3
Ilpo Stenberg <sankari@turska.(none)>
2010-02-12 01:17:40 +0200

    lets make session in first place although it is not needed yet

--- ui/mainpage.py
+++ ui/mainpage.py
@@ -1,10 +1,13 @@
 from model.generator_factory import GeneratorFactory
-from ui.basepage import Page
+from ui.basepage import SessionPage, Page
 
 
-class MainPage(Page):
+class MainPage(Page, SessionPage):
     
     def get_values(self):
+        # note: we need this here otherwise all ajax parts (might)
+        #       generate own sessions
+        self.get_session()
         return { 'complex'        : True,
                  'use_javascript' : True,
                  'generators'     : GeneratorFactory().rated_list(),
