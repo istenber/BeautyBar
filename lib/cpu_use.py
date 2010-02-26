2ccbb0cf3e5cabda5ee4867a1e0582a320d730a1
Ilpo Stenberg <sankari@turska.(none)>
2010-02-26 15:28:04 +0200

    helper script to measure cpu use of generators

---
+++ lib/cpu_use.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import resource
+import time
+import sys
+
+sys.path.append('/home/sankari/dev/beautybar')
+
+from model.generator_factory import GeneratorFactory
+
+
+def TIME_cpu_time():
+    return resource.getrusage(resource.RUSAGE_SELF)[0]
+
+def RESOURCE_cpu_time():
+    return time.clock()
+
+cpu_time = RESOURCE_cpu_time
+
+def measure(diagram):
+    start = cpu_time()
+    diagram.output()
+    end = cpu_time()
+    return (end - start) * 1000
+
+def main():
+    print "CPU Times..."
+    print "---------------------------------------"
+    for diagram in GeneratorFactory().list():
+        diagram.set_range(0, 50)
+        diagram.add_row('a', 10)
+        diagram.add_row('b', 15)
+        diagram.add_row('c', 20)
+        diagram.add_row('d', 30)
+        diagram.add_row('e', 40)
+        diagram.add_row('f', 50)
+        print "%15s   %3.0f ms" % (str(diagram.get_name()),
+                                   measure(diagram))
+    print "---------------------------------------"        
+    
+
+if __name__ == "__main__":
+    main()
