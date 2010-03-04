047e4c296e5f4512b1499230d77f0b0943ce3997
Ilpo Stenberg <sankari@turska.(none)>
2010-03-05 00:30:05 +0200

    generator fixed to new interface

--- generators/nature.py
+++ generators/nature.py
@@ -12,16 +12,10 @@ class Nature(SvgFigGenerator):
         SvgFigGenerator.__init__(self)
         self.bg_image = 1
 
-    def get_defs(self):
-        # TODO: not used!
-        #r = SVG("rect", id="bgrect",
-        #        height=200, width=200, x=0, y=0)
-        #cp = SVG("clipPath", r, id="bgimage")
-        #defs = SVG("defs", cp, id="defs")
-        defs = SVG("defs", id="defs")
-        return defs
-
     def get_elements(self):
+        self.calc(edge_width = 20,
+                  bar_size = 80,
+                  font_size = 13)
         return SVG("g", 
                    self._get_background(),
                    self._get_bars())
@@ -63,35 +57,38 @@ class Nature(SvgFigGenerator):
         # full_width-bar_size_with_space*bar_count-last_bar_space
         # divided by to as we want same amount of space both sides
         i = 30
-        bar_width=30
-        shs = 3
-        for row in self.rows:
-            name = str(row[0])
+        bar_width = self.calc.bar_width
+        shs = 5 - self.get_row_count() / 3
+        fs = self.calc.font_size
+        for i in range(0, self.get_row_count()):
+            name = self.get_row_name(i)
             # TODO: scale value (50.0 -> to something else)
             # 140 is max value that fits in frame, maybe we need
             # a bit smaller... and shadows take space as well
-            value = (row[1] - self.min) * 135.0 / (self.max - self.min)
+            value = self.get_row_value(i) * 135
             # baseline is at 165 (=200-10-25)
             pos = 165-value
+            x = self.calc.left(i) - 5
+            xm = self.calc.middle(i) - 5
             bar = SVG("g",
-                      SVG("rect", id="shadow_" + name, width=bar_width, x=i-shs,
+                      SVG("rect", id="shadow_" + name, width=bar_width, x=x-shs,
                           height=value+shs, y=pos-shs,
                           style="fill:#000000"),
-                      SVG("rect", id="bar_" + name, width=bar_width, x=i,
+                      SVG("rect", id="bar_" + name, width=bar_width, x=x,
                           y=pos, height=value,
                           style="fill:#00ff00"),
-                      Text(i+bar_width/2.0, 180, name, font_size=11,
+                      Text(xm, 180, name, font_size=fs,
                            style="fill:#00ff00; text-anchor:middle;").SVG())
             # TODO: center text!
             bars.append(bar)
-            i += 40
         return bars
 
-    # Gui interface
     def get_description(self):
         return "Theme from nature"
+
     def get_ui_name(self):
         return "Nature theme"
+
     def get_attributes(self):
         # TODO: allow color themes
         # TODO: different size and placed shadows for bars
@@ -99,3 +96,6 @@ class Nature(SvgFigGenerator):
         bg_image = Choice(self, "bg_image", "Background image",
                           ["A", "B", "C"])
         return [bg_image]
+
+    def get_version(self):
+        return 2
