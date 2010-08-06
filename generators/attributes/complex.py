21f3cd12775365a97798783c1d3e5693a467a8dd
sankari <istenber@gmail.com>
2010-08-06 15:21:32 +0300

    force reload option added to attribute caching

--- generators/attributes/complex.py
+++ generators/attributes/complex.py
@@ -6,9 +6,12 @@ MAX_COUNT = 20
 BACKGROUND_IMAGES = None
 
 
-def choices_from_db():
+# TODO: make general caching function
+# TODO: make admin function to reload this automatically?
+# TODO: max count needs to be larger
+def choices_from_db(force_reload=False):
     global BACKGROUND_IMAGES
-    if BACKGROUND_IMAGES is None:
+    if (BACKGROUND_IMAGES is None) or force_reload:
         from ui.image import Image
         objs = Image.gql("WHERE role = :1", "attribute").fetch(MAX_COUNT)
         BACKGROUND_IMAGES = sorted(map(lambda x: x.name.split("/")[1], objs))
